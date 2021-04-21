a7ffd8bf2f81c6cd39cb267d80651a03c0337fda
Vineeth <vineethvrao1@gmail.com>
2021-04-21 18:29:50 +0530

    first commit

---
+++ VolumeHandControl.py
@@ -0,0 +1,91 @@
+import cv2
+import numpy as np
+import time
+import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+################################
+wCam, hCam = 640, 480
+################################
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+detector = htm.HandDetector(detectionCon=0.7)
+
+############
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# print(volume.GetMasterVolumeLevel())
+# print(volume.GetVolumeRange())
+volRange = volume.GetVolumeRange()
+# volume.SetMasterVolumeLevel(-64, None)
+minVol = volRange[0]
+maxVol = volRange[1]
+pVol = volume.GetMasterVolumeLevel()
+print(pVol)
+###############
+vol = 0
+volBar = np.interp(pVol, [minVol, maxVol], [400, 150])
+
+volRectX = 85
+volRectY = 400
+# volBar = 400
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        # print(lmList[4], lmList[8])
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        # print([x1, x2, y1, y2, cx, cy])
+
+        cv2.circle(img, (x1, y1), 20, (255, 0, 0), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 20, (255, 0, 0), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 0), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        # print(length)
+
+        # Hand range 50 - 300
+        # MyHand range 30 - 240
+        handRangeX = 25
+        handRangeY = 210
+        # Volume range -65 - 0
+
+        vol = np.interp(length, [handRangeX, handRangeY], [minVol, maxVol])
+        volBar = np.interp(length, [handRangeX, handRangeY], [400, 150])
+        print(int(length), vol)
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length < 50:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+
+        # test.append(length)
+        # print(min(test), max(test))
+    cv2.rectangle(img, (50, 150), (volRectX, volRectY), (0, 255, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (volRectX,
+                  volRectY), (0, 255, 0), cv2.FILLED)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
+                1, (255, 0, 0), 3)
+
+    cv2.imshow("Img", img)
+    cv2.waitKey(1)
